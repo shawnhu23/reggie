5baf7f8d661060156249d785b2148cb9457d5ca3
shawnhu23 <xiding.hu@mail.mcgill.ca>
2022-07-26 05:51:09 -0400

    use Spring Cache to cache setmeal data

--- src/main/java/com/xhu/reggie/controller/SetmealController.java
+++ src/main/java/com/xhu/reggie/controller/SetmealController.java
@@ -14,6 +14,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -39,6 +41,7 @@ public class SetmealController {
      * @return
      */
     @PostMapping
+    @CacheEvict(value = "setmealCache", allEntries = true)
     public R<String> save(@RequestBody SetmealDto setmealDto) {
         log.info("Setmeal info: {}", setmealDto.toString());
         setmealService.saveWithDish(setmealDto);
@@ -87,6 +90,7 @@ public class SetmealController {
      * @return
      */
     @DeleteMapping
+    @CacheEvict(value = "setmealCache", allEntries = true)
     public R<String> delete(@RequestParam List<Long> ids) {
         log.info("ids: {}", ids);
         setmealService.removeWithDish(ids);
@@ -100,6 +104,7 @@ public class SetmealController {
      * @return
      */
     @GetMapping("/list")
+    @Cacheable(value = "setmealCache", key = "#categoryId + '_' + #status")
     public R<List<Setmeal>> list(Long categoryId, int status) {
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper();
         queryWrapper.eq(categoryId != null, Setmeal::getCategoryId, categoryId);
